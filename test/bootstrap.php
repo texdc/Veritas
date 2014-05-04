25abf5cd1c753209e1ae73b2b58d159b8573c965
George Cooksey <texdc3@gmail.com>
2014-05-04 12:42:31 -0500

    Update Composer, Travis, PHPUnit config

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * bootstrap.php
+ *
+ * @copyright 2014 George D. Cooksey, III
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
 
-chdir(__DIR__ . "/../");
-require_once 'vendor/autoload.php';
+ini_set('error_reporting', E_ALL);
+
+$loader = require 'vendor/autoload.php';
+if (!isset($loader)) {
+    throw new RuntimeException('vendor/autoload.php could not be found.');
+}
+
+$loader->add('VeritasTest\\', __DIR__);
